1a641a44e88768ce2395a7746b1899dddc55daae
Crow18 <ligumacarl@gmail.com>
2020-02-26 15:54:52 +0200

    update

---
+++ 8 methods/methods.java
@@ -0,0 +1,18 @@
+public class methods
+{
+    public static void Name()
+    {
+        System.out.println("Kauthar");
+    }
+
+    public static void LastName()
+    {
+        System.out.println("Schoeman");
+    }
+
+    public static void main(String args[])
+    {
+        Name(); //executes Name();
+        LastName(); //executes LastName();
+    }
+}
---
+++ 8 methods/methods2.java
@@ -0,0 +1,19 @@
+public class methods2
+{
+    public static String Name()
+    {
+        String name = "Kauthar";
+        return name;
+    }
+
+    public static String LastName()
+    {
+        String lastname = "Schoeman";
+        return lastname;
+    }
+
+    public static void main(String args[])
+    {
+        System.out.printf("%s %s\n", Name(), LastName());
+    }
+}
