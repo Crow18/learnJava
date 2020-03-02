35b3cd7ce2cd93a52d61f395ef121e07bd0ba5fe
Crow18 <46410829+Crow18@users.noreply.github.com>
2020-03-02 10:46:45 +0200

    Update file2.java

--- 9 more_classes/more_classes3/file2.java
+++ 9 more_classes/more_classes3/file2.java
@@ -2,9 +2,11 @@ package more_classes3;
 
 public class file2
 {
+    // we will focus on calling methods from objects
     public static void main(String[] args)
     {
         file1 test = new file1();
         test.Outfile();
+        System.out.println(test.num());
     }
 }
